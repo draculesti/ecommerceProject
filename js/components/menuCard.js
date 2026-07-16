8c6c8eb05d2c4b71e832756574f5e226e310c377
Ivonne Garcia <garcia.murcio.ivonne@gmail.com>
2026-07-16 15:51:36 -0600

    menu updated name of json

--- js/components/menuCard.js
+++ js/components/menuCard.js
@@ -1,14 +1,14 @@
 
 export const menuCard = (item) =>`
   <div class="col-12 col-sm-6 col-md-4 col-lg-4 mb-4">
-    <div class="card shadow h-100 menu-card" data-id="${item.id}">
-      <img src="${item.img}" alt="${item.name}">
+    <div class="card shadow h-100 menu-card" data-id="${item.id_platillo}">
+      <img src="${item.imagen}" alt="${item.nombre}">
       <div class="overlay">
         <div>
-          <h5 class="card-title">${item.name}</h5>
-          <p class="price">${item.price}</p>
+          <h5 class="card-title">${item.nombre}</h5>
+          <p class="price">${item.precio}</p>
           <p class="card-text">
-            ${item.description}
+            ${item.descripcion}
           </p>
         </div>
         <div class="card-buttons">
@@ -18,6 +18,6 @@ export const menuCard = (item) =>`
         </div>
       </div>
     </div>
-      <h2 class="text-center">${item.name}</h2>
+      <h2 class="text-center">${item.nombre}</h2>
   </div>
 `; 
